ce4127c5732963e35cb58a8192ccf353a1c68548
pranay-majumder <pranay.majumder17@gmail.com>
2025-11-09 02:37:09 +0530

    Front End Created

---
+++ app.py
@@ -0,0 +1,18 @@
+## Create Virtual Enviorment
+## python -m venv env
+## venv/Scripts/Activate
+## streamlit run app.py
+
+
+import streamlit as st ## type:ignore
+
+st.title('CampusX')
+
+col1,col2=st.columns(2)
+
+with col1:
+    st.image('image.jpeg')
+
+with col2:
+    st.header('Campusx is an Online Platform')
+
